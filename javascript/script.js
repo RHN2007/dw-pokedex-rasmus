2e37068aab1adb000d09a6524a3efe082d8362a0
Rasmus Nielsen <rasmushulbaknielsen@gmail.com>
2026-09-22 10:45:42 +0200

    tilføjet fetch functionalitet til script.js så vi kan hente pokemons fra pokemon apiet.

--- javascript/script.js
+++ javascript/script.js
@@ -1,14 +1,21 @@
 import Header from "./components/Header.js";
+import Main from "./components/Main.js";
 
 const rootElement = document.querySelector("#root")
-
+let pokemons = []
 
 
 function render() {
+    rootElement.innerHTML = ""
     rootElement.append(Header())
+    rootElement.append(Main(pokemons))
 }
 
-function init() {
+async function init() {
+    const pokemonURL = "https://pokeapi.co/api/v2/pokemon?limit=151"
+    let response = await fetch(pokemonURL)
+    let data = await response.json()
+    pokemons = data.results
     render()
 }
 
