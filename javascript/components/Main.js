95ecb18f6a1d5aed38ade79ccc7d00a27ba991b3
Rasmus Nielsen <rasmushulbaknielsen@gmail.com>
2026-09-24 13:22:33 +0200

    Tilføjet mere javascript til details sidens main, du viser den ting som vægt, højde og dens moves.

--- javascript/components/Main.js
+++ javascript/components/Main.js
@@ -9,7 +9,7 @@ export default function Main(pokemons) {
         ${pokemons.map(function(pokemon) {
             return `
                 <li class="card">
-                    <a href="detail.html?id=${getIDfromURL(pokemon.url)}&name=${pokemon.name}" class="card">
+                    <a href="detail.html?id=${getIDfromURL(pokemon.url)}" class="card">
                         <p class="card__id">#${getIDfromURL(pokemon.url).padStart(3, "0")}</p>
                         <img class="card__image" src="${basePokemonURL + getIDfromURL(pokemon.url) + ".png" }" alt="${pokemon.name}">
                         <h2 class="card__name">${pokemon.name}</h2>
@@ -41,6 +41,37 @@ export function detailsMain (pokemon) {
             `
         }).join("")}
     </ul>
+    <section class="about">
+        <h2 style="color:var(--${pokemon.types[0].type.name}-type-color);">About</h2>
+        <ul class="about__list">
+            <li class="about__list__item">
+                <div class="item__info">
+                    <img src="../assets/weight.svg" alt="Weight">
+                    <p>${pokemon.weight / 10} kg</p>
+                </div>
+                <small>Weight</small>
+            </li>
+            <li class="about__list__item">
+                <div class="item__info">
+                    <img src="../assets/height.svg" alt="Height">
+                    <p>${pokemon.height / 10} m</p>
+                </div>
+                <small>Height</small>
+            </li>
+            <li class="about__list__item">
+                <div class="item__info">
+                    ${pokemon.abilities.map(function (ability) {
+                    return `
+                    <p>${ability.ability.name}</p>
+                    `
+                }).join("")}
+                </div>
+                <small>Moves</small>
+            </li>
+        </ul>
+    </section>
+
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque, vitae?</p>
     </div>
     `
     return mainElement
