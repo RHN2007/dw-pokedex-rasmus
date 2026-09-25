5fb88ac20ee105a1559fb731e1347c4a425c2a30
Rasmus Nielsen <rasmushulbaknielsen@gmail.com>
2026-09-25 11:17:36 +0200

    Tilføjet Search() i vores script.js så vi faktisk kan bruge vores søge funktion

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,7 @@
 import Header from "./components/Header.js";
 import Main from "./components/Main.js";
 import fetchData from "./Functionality/Fetch.js";
+import Search from "./Functionality/Search.js";
 
 const rootElement = document.querySelector("#root")
 let pokemons = []
@@ -10,11 +11,11 @@ function render() {
     rootElement.innerHTML = ""
     rootElement.append(Header())
     rootElement.append(Main(pokemons))
+    Search()
 }
 
 async function init() {
     pokemons = (await fetchData("?limit=151")).results
-    console.log(pokemons)
     render()
 }
 
