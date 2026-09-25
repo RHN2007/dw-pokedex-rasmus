34d4172cf9195e9abe17ef4346d3abb43e8a8588
Rasmus Nielsen <rasmushulbaknielsen@gmail.com>
2026-09-25 12:31:31 +0200

    tilføjet ID search funktionalitet.

--- javascript/Functionality/Search.js
+++ javascript/Functionality/Search.js
@@ -1,22 +1,32 @@
-export default function Search () {
+export default function Search() {
     let search = document.querySelector("#search")
     let allCards = document.querySelectorAll(".card")
-    
+
     console.log(search.value)
     search.addEventListener("input", () => {
         let searchInput = search.value.toLowerCase()
         allCards.forEach(element => {
-            
+
+            if (isNaN(searchInput) == true) {
+                if (element.dataset.name.includes(searchInput)) {
+                    element.classList.remove("hidden")
+                } else {
+                    element.classList.add("hidden")
+                }
+            } else {
+                if (element.dataset.id.includes(searchInput)) {
+                    element.classList.remove("hidden")
+                } else {
+                    element.classList.add("hidden")
+                }
+            }
+
             if (searchInput == "") {
                 element.classList.remove("hidden")
                 return
             }
 
-            if (element.dataset.name.includes(searchInput)) {
-                element.classList.remove("hidden")
-            } else {
-                element.classList.add("hidden")
-            }
+
         });
 
     })
